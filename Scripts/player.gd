352859060e3e698a6bafa05c2d5633529860f302
MarioValverdeBerenjeno <104562518+MarioValverdeBerenjeno@users.noreply.github.com>
2024-10-06 13:21:15 +0200

    audio

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,7 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += gravity * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
