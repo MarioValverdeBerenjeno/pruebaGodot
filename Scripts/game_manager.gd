4ff3b811083425c4fcbfc0b6d0608fb942b7e860
MarioValverdeBerenjeno <104562518+MarioValverdeBerenjeno@users.noreply.github.com>
2024-10-06 16:36:35 +0200

    score

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var score = 0
+@onready var labelNode: Label = $"../Score/UI/Base/Label"
 
 func add_point():
 	score += 1
+	labelNode.text = "Score: " + str(score)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+const DASH_VELOCITY = 400.0
+var dashing = false
+var can_dash = true
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -16,10 +23,12 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$can_dash_timer.start()
 	
 	if direction > 0:
 		animated_sprite.flip_h = false
@@ -27,18 +36,27 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 		
 	if is_on_floor():
-		if direction == 0:
+		if direction == 0 and !dashing:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-		
-	
 	
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction * DASH_VELOCITY
+		else:
+			velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+# Stop dashing
+func _on_dash_time_timeout() -> void:
+	dashing = false
+
+
+func _on_can_dash_timer_timeout() -> void:
+	can_dash = true
