c2996a831b9bd6d23e01b9ec054b2fe900dbd377
MarioValverdeBerenjeno <104562518+MarioValverdeBerenjeno@users.noreply.github.com>
2024-10-06 16:52:20 +0200

    fin

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,8 +1,14 @@
 extends Node
 
 var score = 0
+var score_required = 9
 @onready var labelNode: Label = $"../Score/UI/Base/Label"
 
 func add_point():
 	score += 1
 	labelNode.text = "Score: " + str(score)
+	if score == score_required:
+		win()
+
+func win():
+	get_tree().reload_current_scene()
